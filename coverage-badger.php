f63945e68b3e705b5fcf962c7d2fed371bcf62a3
Joseph Schilz <joseph@schilz.org>
2017-07-21 12:59:59 -0700

    Add color to badge template.

--- coverage-badger.php
+++ coverage-badger.php
@@ -23,4 +23,22 @@ $template = file_get_contents(__DIR__ . '/templates/flat.svg');
 
 $template = str_replace('{{ total }}', $coverage, $template);
 
+$color = '#a4a61d';      // Default Gray
+if ($coverage < 40) {
+    $color = '#e05d44';  // Red
+} elseif($coverage < 60) {
+    $color = '#fe7d37';  // Orange
+} elseif($coverage < 75) {
+    $color = '#dfb317';  // Yellow
+} elseif($coverage < 90) {
+    $color = '#a4a61d';  // Yellow-Green
+} elseif($coverage < 95) {
+    $color = '#97CA00';  // Green
+} elseif ($coverage <= 100) {
+    $color = '#4c1';     // Bright Green
+}
+
+$template = str_replace('{{ total }}', $coverage, $template);
+$template = str_replace('{{ color }}', $color, $template);
+
 file_put_contents($outputFile, $template);
